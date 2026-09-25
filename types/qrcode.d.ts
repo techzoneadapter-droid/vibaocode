fc28b0bb77a706c3602e494d530e82374ab59625
techzoneadapter-droid <techzone.adapter@gmail.com>
2026-09-25 15:37:13 +0700

    chore: add QR code module types

---
+++ types/qrcode.d.ts
@@ -0,0 +1,16 @@
+declare module "qrcode" {
+  export type QRCodeToDataURLOptions = {
+    errorCorrectionLevel?: "L" | "M" | "Q" | "H";
+    margin?: number;
+    width?: number;
+    color?: {
+      dark?: string;
+      light?: string;
+    };
+  };
+
+  export function toDataURL(
+    text: string,
+    options?: QRCodeToDataURLOptions,
+  ): Promise<string>;
+}
